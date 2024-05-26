56257299199bfc1ab69da4b6b5ca99f204a91d94
nourmedini1 <mohamednour.medini@etudiant-isi.utm.tn>
2024-05-26 13:15:00 +0100

    fix/fixed typo in ComplexOperationResponse

--- src/routes/complex/domain/models/complex_operation_response.py
+++ src/routes/complex/domain/models/complex_operation_response.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel
 class ComplexOperationResponse(BaseModel) :
     z1 : str
     z2 : str
-    PolarZ1 : str
-    PolarZ2 : str
+    polarZ1 : str
+    polarZ2 : str
     algebraicResult : str
     polarResult : str
