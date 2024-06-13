ea5bfa0de9ece7c178645166a1d1b7c543ac3c1a
nourmedini1 <mohamednour.medini@etudiant-isi.utm.tn>
2024-06-13 07:33:36 +0100

    included the routers in the main app file

--- src/app.py
+++ src/app.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI 
+from .routes.complex.resource.complex_resource import complexRouter
+from .routes.derivatives.resource.derivatives_resource import derivativesRouter
+from .routes.differential_equations.resource.differential_equations_resource import differentialEquationsRouter
 
 app = FastAPI()
+
+app.include_router(complexRouter)
+app.include_router(derivativesRouter)
+app.include_router(differentialEquationsRouter)
 
 @app.get("/") 
 def read_root():
