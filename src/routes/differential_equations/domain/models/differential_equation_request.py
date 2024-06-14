9ae0fdfb522dfde61bfd99f18cf4421b9fd27222
nourmedini1 <mohamednour.medini@etudiant-isi.utm.tn>
2024-06-14 15:30:57 +0100

    fixed issues in the differential equations service and optimized the request format

--- src/routes/differential_equations/domain/models/differential_equation_request.py
+++ src/routes/differential_equations/domain/models/differential_equation_request.py
@@ -1,17 +1,12 @@
 from pydantic import BaseModel 
 from typing import List,Optional
+from routes.differential_equations.domain.models.initial_condition import InitialCondition
 
 
 class DifferentialEquationRequest(BaseModel):
     variable : str
-    firstCoefficient : Optional[str]
-    secondCoefficient : Optional[str]
-    thirdCoefficient : Optional[str]
-    fourthCoefficient : Optional[str]
-    firstInitialCondition : Optional[List[str]]
-    secondInitialCondition : Optional[List[str]] 
-    thirdInitialCondition : Optional[List[str]] 
-    fourthInitialCondition : Optional[List[str]]
+    coefficients : List[Optional[str]]
+    initialConditions : List[Optional[InitialCondition]]
     constant : Optional[str]
     rightHandSide : str = "0"
 
--- src/routes/differential_equations/domain/models/differential_equation_response.py
+++ src/routes/differential_equations/domain/models/differential_equation_response.py
@@ -3,6 +3,5 @@ from typing import List
 
 class DifferentialEquationResponse(BaseModel):
     equation : str 
-    initialConditions : List[List[str]] = [["",""]]
     solution : str
     
