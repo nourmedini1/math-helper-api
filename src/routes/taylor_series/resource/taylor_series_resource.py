4726c8470ecd975862a3b88c59a7cd3185a6736f
nourmedini1 <mohamednour.medini@etudiant-isi.utm.tn>
2024-06-15 18:09:47 +0100

    created the resource layer for the taylor series feature

---
+++ src/routes/taylor_series/resource/taylor_series_resource.py
@@ -0,0 +1,16 @@
+from fastapi import APIRouter,HTTPException
+from routes.taylor_series.service.taylor_series_service import TaylorSeriesService
+from routes.taylor_series.domain.models.taylor_series_request import TaylorSeriesRequest
+from routes.taylor_series.domain.models.taylor_series_response import TaylorSeriesResponse
+
+
+CONTEXT_PATH = "taylor-series"
+taylorSeriesRouter = APIRouter(prefix=f"/api/v1/{CONTEXT_PATH}", tags=["taylor series"])
+
+@taylorSeriesRouter.post("/taylor-series", response_model=TaylorSeriesResponse, status_code=200)
+async def taylor_series(request : TaylorSeriesRequest) -> TaylorSeriesResponse :
+    try :
+        response = TaylorSeriesService().taylorSeries(request)
+        return response
+    except Exception as e:
+        raise HTTPException(status_code=400,detail= str(e))
