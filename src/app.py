427c0acb1697087c346f768742e6b857cfee2869
nourmedini1 <mohamednour.medini@etudiant-isi.utm.tn>
2024-06-16 17:57:00 +0100

    registered the sum api router

--- src/app.py
+++ src/app.py
@@ -5,6 +5,7 @@ from routes.differential_equations.resource.differential_equations_resource impo
 from routes.integrals.resource.integrals_resource import integralsRouter
 from routes.limits.resource.limits_resource import limitsRouter
 from routes.taylor_series.resource.taylor_series_resource import taylorSeriesRouter
+from routes.sum.resource.sum_resource import sumRouter
 
 app = FastAPI()
 
@@ -14,6 +15,8 @@ app.include_router(differentialEquationsRouter)
 app.include_router(integralsRouter)
 app.include_router(limitsRouter)
 app.include_router(taylorSeriesRouter)
+app.include_router(sumRouter)
+
 
 @app.get("/") 
 def math_helper_root_path():
