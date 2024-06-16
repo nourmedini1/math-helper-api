bce299a9f8b58022f213cf6f8bbb1663540c8af8
nourmedini1 <mohamednour.medini@etudiant-isi.utm.tn>
2024-06-16 13:21:33 +0100

    created the resource layer for the sum feature

---
+++ src/routes/sum/service/sum_service.py
@@ -0,0 +1,67 @@
+import sympy as smp
+from typing import Union
+from routes.sum.domain.models.sum_request import SumRequest
+from routes.sum.domain.models.sum_response import SumResponse
+from utils.parse_input import InputParser
+
+class SumServiceMeta(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(SumServiceMeta, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+    
+class SumService(metaclass= SumServiceMeta) :
+
+    def _setupLimits(self,lowerLimit : Union[str,int,float], upperLimit : Union[str,int,float]) :
+        if lowerLimit == "-oo" : 
+            lowerLimit = -smp.oo
+        if upperLimit == "oo" :
+            upperLimit = smp.oo
+        return lowerLimit,upperLimit
+
+    def _setupParameters(self,request : SumRequest) -> tuple[str] :
+        variable = smp.Symbol(request.variable)
+        expression = smp.sympify(InputParser.parse_expression(request.expression))
+        lowerLimit,upperLimit = self._setupLimits(request.lowerLimit,request.upperLimit)
+        return expression,variable,lowerLimit,upperLimit
+    
+    def symbolicSum(self, request : SumRequest) -> SumResponse :        
+        expression,variable,lowerLimit,upperLimit = self._setupParameters(request)
+        summation = smp.summation(expression,(variable,lowerLimit,upperLimit))  
+        if summation.is_convergent() :
+            convergent = True
+            try :
+                result = smp.simplify(smp.summation(expression,(variable,lowerLimit,upperLimit)))
+            except Exception:
+                result = "no numeric form found"
+        else :
+            convergent = False
+            result = ""
+        return SumResponse(
+            convergent= convergent,
+            summation = smp.latex(summation),
+            result = result
+            )
+
+        
+    def numericSum(self,request : SumRequest) -> SumResponse :
+        expression,variable,lowerLimit,upperLimit = self._setupParameters(request)
+        summation = smp.summation(expression,(variable,lowerLimit,upperLimit))
+        if summation.is_convergent() :
+            convergent = True
+            try :
+                result = smp.N(smp.summation(expression,(variable,lowerLimit,upperLimit)))
+            except Exception:
+                result = "no numeric form found"
+        else :
+            convergent = False
+            result = ""
+        return SumResponse(
+            convergent= convergent,
+            summation = smp.latex(summation),
+            result = result
+            )
+
+        
