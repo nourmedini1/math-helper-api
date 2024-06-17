4c21ed3c1b447caaa142d7633d9bb2c7512652ff
nourmedini1 <mohamednour.medini@etudiant-isi.utm.tn>
2024-06-17 09:34:47 +0100

    made the result optional for error handling in the response for the linear system feature

--- src/routes/linear_systems/domain/models/linear_system_request.py
+++ src/routes/linear_systems/domain/models/linear_system_request.py
@@ -1,7 +1,7 @@
 from pydantic import BaseModel
-from typing import List
+from typing import List,Union
 
 class LinearSystemRequest(BaseModel):
     equations : List[str]
     variables : List[str]
-    righHandSide : List[str]
+    righHandSide : List[Union[str,float,int]]
