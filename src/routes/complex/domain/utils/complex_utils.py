c4544dea2fb1b602958b9b114a9303930ae4e15b
nourmedini1 <mohamednour.medini@etudiant-isi.utm.tn>
2024-11-10 07:27:06 +0100

    fixed issue with imaginary numbers in complex analysis

--- src/routes/complex/domain/utils/complex_utils.py
+++ src/routes/complex/domain/utils/complex_utils.py
@@ -1,25 +1,24 @@
 import sympy as smp
 
-class ComplexUtils :
+class ComplexUtils:
 
     @staticmethod
-    def latexifyComplexNumber(real : str,imaginary : str) -> str :
-        return smp.latex(real + smp.I*imaginary)
+    def latexifyComplexNumber(real: str, imaginary: str) -> str:
+        real_expr = smp.sympify(real)
+        imaginary_expr = smp.sympify(imaginary)
+        return smp.latex(real_expr) + ' + ' + smp.latex(imaginary_expr) + 'i'
         
     @staticmethod
-    def createPolarForm(real : str , imaginary : str) -> str :
-        z = real + imaginary * smp.I 
+    def createPolarForm(real: str, imaginary: str) -> str:
+        real_expr = smp.sympify(real)
+        imaginary_expr = smp.sympify(imaginary)
+        z = real_expr + imaginary_expr * smp.I 
         argument = smp.ratsimp(smp.arg(z))
         modulus = smp.ratsimp(smp.Abs(z))
         return smp.latex(modulus * smp.exp(smp.I * argument))
     
     @staticmethod
-    def createAlgebraicForm(real : str,imaginary : str) -> str:
-        return real + imaginary * smp.I
-
-
-
-
-
-
-    
+    def createAlgebraicForm(real: str, imaginary: str) -> str:
+        real_expr = smp.sympify(real)
+        imaginary_expr = smp.sympify(imaginary)
+        return smp.latex(real_expr + imaginary_expr * smp.I)
